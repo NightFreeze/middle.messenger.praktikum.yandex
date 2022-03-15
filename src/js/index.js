aed090c57363469b797f548f28c416f4a107af80
Anna Danshina <annetteblackat@gmail.com>
2022-03-16 00:07:31 +0300

    добавила стили, sign in, sign up, компоненты

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
-import myTest from '../components/test.tmpl.pug';
+// import myTest from '../components/test.tmpl.pug';
 
-document.querySelector('#root').innerHTML = myTest({name: 'QWERTY'});
+// document.querySelector('#root').innerHTML = myTest({name: 'QWERTY'});
+
+import signIn from '../screens/signIn/signIn.tmpl.pug';
+import signUp from '../screens/signUp/signUp.tmpl.pug';
+import errorPage from '../screens/errors/errorScreen.tmpl.pug';
+import chat from '../screens/chat/chat.tmpl.pug'
+
+// document.querySelector('#root').innerHTML = errorPage({ errorCode: '500', errorText: 'уже фиксим!' });
+document.querySelector('#root').innerHTML = signUp();
