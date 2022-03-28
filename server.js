10686beaefcb213f3e4be670646b4def3b07f2df
Anna Danshina <annetteblackat@gmail.com>
2022-03-28 23:06:52 +0300

    fixes for strint 1

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(express.static('./dist'));
 
