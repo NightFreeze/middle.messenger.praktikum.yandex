07543bae2493df6c4c98d8003188a6d9d766713c
Anna Danshina <annetteblackat@gmail.com>
2022-04-12 17:33:04 +0300

    изменила путь к сборке

--- server.ts
+++ server.ts
@@ -3,9 +3,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static('./dist'));
+app.use(express.static(`${__dirname}/dist/`));
 
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);
